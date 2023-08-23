154e117c97fb200068ac656097e9a2ed4b309bec
mojtabasharafiyeh <61268429+mojtabasharafiyeh@users.noreply.github.com>
2023-08-23 22:34:11 +0330

    end the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,48 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'my-first-projec';
+  length = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  password = '';
+  onChangeLength(value: string) {
+    const parsed = parseInt(value);
+    if (parsed !== null) {
+      this.length = parsed;
+    }
+  }
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+  onButtonClick() {
+    const numbers = '1234567890';
+    const symbols = '!@#$%^&*()_+';
+    const letter = 'zxcvbnmasdfghjklqwertyuiop';
+    let included = '';
+    if (this.includeLetters) {
+      included += letter;
+    }
+    if (this.includeNumbers) {
+      included += numbers;
+    }
+    if (this.includeSymbols) {
+      included += symbols;
+    }
+    let pass = '';
+    for (let i = 0; i <= this.length; i++) {
+      const index = Math.floor(Math.random() * included.length);
+      pass += included[index];
+    }
+    this.password = pass;
+  }
 }
